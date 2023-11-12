11e8b6cf74608f6fc8910560d7c29f238dcc33a0
baekseungchan1117 <bsc7386@gmail.com>
2023-11-12 17:45:52 +0900

    Validation, default 값 설정

--- src/main/java/com/qortmdcks/crudpra/payload/CategoryCarDto.java
+++ src/main/java/com/qortmdcks/crudpra/payload/CategoryCarDto.java
@@ -1,12 +1,17 @@
 package com.qortmdcks.crudpra.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
 public class CategoryCarDto {
     private Long id;
 
+    @NotEmpty
+    @Size(min = 2, message = "car name")
     private String name;
 
-    private String color;
+    private String color = "white";
 }
