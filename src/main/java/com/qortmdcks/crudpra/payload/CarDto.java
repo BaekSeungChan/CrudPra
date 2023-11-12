a5fd4107a20f6155908b3abf66fc8cf77420e28d
baekseungchan1117 <bsc7386@gmail.com>
2023-11-12 17:01:11 +0900

    Validation, default 값 설정

--- src/main/java/com/qortmdcks/crudpra/payload/CarDto.java
+++ src/main/java/com/qortmdcks/crudpra/payload/CarDto.java
@@ -1,12 +1,16 @@
 package com.qortmdcks.crudpra.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CarDto {
     private Long id;
 
+    @NotEmpty
+    @Size(min = 2, message = "Car name should have at least 2characters")
     private String name;
 
-    private Integer price;
+    private Integer price = 0;
 }
