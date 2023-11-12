6181f4aeed2b2e6d8afc660ebeb04eddf798d1b6
baekseungchan1117 <bsc7386@gmail.com>
2023-11-12 16:13:41 +0900

    Bean 설정 및 ModelMapper 설정

--- src/main/java/com/qortmdcks/crudpra/CrudPraApplication.java
+++ src/main/java/com/qortmdcks/crudpra/CrudPraApplication.java
@@ -1,10 +1,17 @@
 package com.qortmdcks.crudpra;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudPraApplication {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return modelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CrudPraApplication.class, args);
