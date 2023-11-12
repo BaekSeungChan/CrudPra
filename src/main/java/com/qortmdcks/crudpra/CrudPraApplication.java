cb88e45309c29f6547e3342b4b96d7a4dca916c3
baekseungchan1117 <bsc7386@gmail.com>
2023-11-12 16:09:33 +0900

    Initial commit

---
+++ src/main/java/com/qortmdcks/crudpra/CrudPraApplication.java
@@ -0,0 +1,13 @@
+package com.qortmdcks.crudpra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qortmdcks/crudpra/CrudPraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qortmdcks.crudpra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
